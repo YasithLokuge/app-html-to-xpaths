fa77ae4e546ec4c8aedaab52460b08af7e854dc7
Yasith Lokuge <ylokuge@yasith-lokuges-macbook-pro.local>
2021-01-20 21:48:36 +1100

    gitignore

--- handler.js
+++ handler.js
@@ -3,27 +3,36 @@ const chromium = require('chrome-aws-lambda');
 
 module.exports.toXpaths = async (event, context, callback) => {
     const url = event.queryStringParameters.url;
-    const browser = await chromium.puppeteer.launch({
-            args: chromium.args,
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath,
+    let statusCode = 200;
+    let result = "";
+    console.log(url);
+    if(validURL(url)) {
+        const browser = await chromium.puppeteer.launch({
+                args: chromium.args,
+                defaultViewport: chromium.defaultViewport,
+                executablePath: await chromium.executablePath,
             headless: chromium.headless,
             ignoreHTTPSErrors: true,
-    });
+        });
 
-    const page = await browser.newPage();
-    await page.goto(url);
+            const page = await browser.newPage();
+            await page.goto(url);
 
-    await page.addScriptTag({ path :"html-to-xpaths.js"});
-    const jsHandle = await page.evaluateHandle(() => {
-        return htmlElementToXpaths(document.documentElement);
-    });
+            await page.addScriptTag({ path :"html-to-xpaths.js"});
+            const jsHandle = await page.evaluateHandle(() => {
+                return htmlElementToXpaths(document.documentElement);
+        });
 
-    const result = await page.evaluate(e => e, jsHandle);
-    await browser.close();
+            result = await page.evaluate(e => e, jsHandle);
+            await browser.close();
+    } else {
+        statusCode = 400;
+        result = "Invalid Url";
+    }
+
 
     var response = {
-        "statusCode": 200,
+        "statusCode": statusCode,
         "headers": {
             'Access-Control-Allow-Origin': 'https://yasithlokuge.github.io',
             'Access-Control-Allow-Credentials': true
@@ -33,3 +42,13 @@ module.exports.toXpaths = async (event, context, callback) => {
     };
     callback(null, response);
 };
+
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
+}
